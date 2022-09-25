55ad9353aa084f834e4c80a5231b03383885cef0
Antonio Junio Pardim <antoniojrpardim@gmail.com>
2022-09-25 17:33:35 -0300

    Formulario Login e inclusão da sub rota

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,10 +1,15 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent}
+  {path:'', component: HomeComponent,
+    children: [
+      {path: '', component: LoginComponent}
+    ]}
 ];
+// children: informa uma rota dentro de um RouterOutlet pertencente aquele component
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
